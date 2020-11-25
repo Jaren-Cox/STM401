6f86b307bab929a9ced5cc42cd41adabb566f59b
Jaren-Cox <73719758+Jaren-Cox@users.noreply.github.com>
2020-11-25 12:50:28 -0500

    Add files via upload

---
+++ Correct_4.7.py
@@ -0,0 +1,20 @@
+inp=input('Enter your Score: ')
+try:
+    gpa=float(inp)
+except:
+    inval=-1
+try:
+    if gpa > 1.5:
+        print('Unrealistic Grade')
+    elif gpa >=0.9:
+       print('Grade: A')
+    elif gpa >=0.8:
+      print('Grade: B')
+    elif gpa >=0.7:
+      print('Grade: C')
+    elif gpa >=0.6:
+        print('Grade: D')
+    elif gpa <0.6:
+     print('Grade: F')
+except:
+    print('Invalid input')
